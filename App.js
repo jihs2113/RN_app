a15cc6d7bcdb5688a198562ace5fa124a7f1322c
jhkim <jhkim@nkia.co.kr>
2025-02-11 10:58:34 +0900

    feat: #3 기획에 맞게 UI 구현및 style 적용              컴포넌트 및 props 적용과 function style 적용
         - ScrollView 컴포넌트 적용
         - pagingEnabled: 화면 단위로 스크롤 적용
         - showsHorizontalScrollIndicator={false} : 스크롤 넘어가는 부분 숨김처리
         - Dimensions.get("window").width : 화면 크기 변수로 구조분해할당

--- App.js
+++ App.js
@@ -1,6 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from "react";
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Dimensions, Text, View, ScrollView } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get("window").width;
 
 export default function App() {
   return (
@@ -8,14 +10,29 @@ export default function App() {
       <View style={styles.city}>
         <Text style={styles.cityName}>Seoul</Text>
       </View>
-      <View style={styles.weather}>
+      <ScrollView 
+      pagingEnabled
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      style={styles.weather}>
         <View style={styles.day}>
           <Text style={styles.temp}>27</Text>
           <Text style={styles.description}>Sunny</Text>
         </View>
-      </View>
+         <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+         <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+         <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
-
   );
 }
 
@@ -30,18 +47,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   cityName:{
-    fontSize: 68,
+    fontSize: 58,
     fontWeight: '500',
   },
   weather:{
-    flex:3,
   },
   day:{
-    flex:1,
+    width: SCREEN_WIDTH,
     alignItems: 'center',
   },
   temp:{
     marginTop: 50,
+    fontWeight: '600',
     fontSize: 178,
   },
   description:{
